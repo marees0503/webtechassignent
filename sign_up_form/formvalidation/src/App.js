8bf3cec6d9d39d002197e3bd80188e39faa96bae
marees0503 <mareeswaranbemech@gmail.com>
2022-09-11 18:21:41 +0530

    first commit

---
+++ sign_up_form/formvalidation/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import NewForm from "./components/Newfrom"
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ sign_up_form/formvalidation/src/components/from.jsx
@@ -0,0 +1,135 @@
+import { useState, useEffect } from "react";
+import "./Form.css"
+
+function App() {
+  const initialValues = { username: "", email: "", password: "",gender:"",phone:"" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormValues({ ...formValues, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormErrors(validate(formValues));
+    setIsSubmit(true);
+  };
+
+  useEffect(() => {
+    console.log(formErrors);
+    if (Object.keys(formErrors).length === 0 && isSubmit) {
+      console.log(formValues);
+    }
+  }, [formErrors]);
+  const validate = (values) => {
+    const errors = {};
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+    var pattern = new RegExp(/^[0-9\b]+$/);
+    if (!values.username) {
+      errors.username = "Username is required!";
+    }
+    if (!values.email) {
+      errors.email = "Email is required!";
+    } else if (!regex.test(values.email)) {
+      errors.email = "This is not a valid email format!";
+    }
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (values.password.length < 4) {
+      errors.password = "Password must be more than 4 characters";
+    } else if (values.password.length > 20) {
+      errors.password = "Password cannot exceed more than 10 characters";
+    }
+    if(!values.gender || values.gender==="Select"){
+        errors.gender="Please select a gender"
+    }
+    if(!pattern.test(values.phone)){
+        errors.phone="Please enter phone number "
+    }else if(values.phone.length!=10){
+        errors.phone="Please enter 10 digit number"
+    }
+    return errors;
+  };
+
+  return (
+    <div className="container">
+      {Object.keys(formErrors).length === 0 && isSubmit ? (
+        <div className="ui message success">Signed in successfully</div>
+      ) : (
+        <pre>{JSON.stringify(formValues, undefined, 2)}</pre>
+      )}
+
+      <form onSubmit={handleSubmit}>
+        <h1>Login Form</h1>
+        <div className="ui divider"></div>
+        <div className="ui form">
+          <div className="field">
+            <label>Username</label>
+            <input
+              type="text"
+              name="username"
+              placeholder="Username"
+              value={formValues.username}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.username}</p>
+          <div className="field">
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              value={formValues.email}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.email}</p>
+          <div className="field">
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formValues.password}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.password}</p>
+          <div className="field">
+          <label>Gender</label>
+            <select 
+            name="gender" 
+            id="gender" 
+            onChange={handleChange}
+            value={formValues.gender}
+            >
+                 <option value="Select">Select</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+            </select>
+          </div>
+          <p>{formErrors.gender}</p>
+          <div className="field">
+          <label>Phone</label>
+            <input
+              type="text"
+              name="phone"
+            //   country={"india"}
+              value={formValues.phone}
+              onChange={handleChange}
+            />
+          </div>
+          <p>{formErrors.phone}</p>
+
+          <button className="fluid ui button blue">Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
